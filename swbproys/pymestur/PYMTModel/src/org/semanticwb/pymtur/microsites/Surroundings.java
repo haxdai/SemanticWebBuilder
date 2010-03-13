3126a05d55696acf064f79fbde2f3a9822d297ba
redentor <JRedentor@users.noreply.github.com>
2010-03-13 05:39:45 +0000

    Con modificaciones para sitios premier

--- swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/microsites/Surroundings.java
+++ swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/microsites/Surroundings.java
@@ -11,9 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.semanticwb.Logger;
 import org.semanticwb.SWBUtils;
+import org.semanticwb.model.WebPage;
 import org.semanticwb.portal.api.GenericResource;
 import org.semanticwb.portal.api.SWBParamRequest;
 import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.community.MicroSiteType;
+import org.semanticwb.pymtur.MiPymeSite;
+import org.semanticwb.pymtur.MiPymeSitePlus;
+import org.semanticwb.pymtur.MicroSitePyme;
 
 /**
  *
@@ -26,7 +31,25 @@ public class Surroundings extends GenericResource{
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
 
-        RequestDispatcher dis = request.getRequestDispatcher("/work/models/etour/jsp/pymestur/microsite/surroundings.jsp");
+        RequestDispatcher dis = null;
+        WebPage wp = paramRequest.getWebPage();
+        WebPage community = null;
+        String path = null;
+        if (wp instanceof MicroSitePyme) {
+            community = wp;
+            System.out.println("Es instancia de MicroSitePyme");
+        } else {
+            community = wp.getParent();
+            System.out.println("No es instancia de MicroSitePyme");
+        }
+        String siteUri = ((MicroSitePyme) community).getType().getURI();
+        
+        if (MicroSiteType.ClassMgr.getMicroSiteType("MiPymeSite", wp.getWebSite()).getURI().equals(siteUri)) {
+            path = "/work/models/etour/jsp/pymestur/microsite/surroundings.jsp";
+        } else if (MicroSiteType.ClassMgr.getMicroSiteType("MiPymeSitePlus", wp.getWebSite()).getURI().equals(siteUri)) {
+            path = "/work/models/etour/jsp/pymestur/premier/surroundings.jsp";
+        }
+        dis = request.getRequestDispatcher(path);
         try {
             request.setAttribute("paramRequest", paramRequest);
             dis.include(request, response);
@@ -36,3 +59,16 @@ public class Surroundings extends GenericResource{
     }
 
 }
+/*
+         WebPage community = null;
+        WebPage currentpage = (WebPage) request.getAttribute("webpage");
+        if(currentpage == null) {
+            currentpage = paramRequest.getWebPage();
+        }
+        if(currentpage instanceof MicroSitePyme) {
+            community = currentpage;
+        }else {
+            community = currentpage.getParent();
+        }
+        MicroSitePyme ms = (MicroSitePyme)community;
+*/
