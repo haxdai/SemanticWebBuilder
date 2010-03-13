2dac752c7a6ec964e30335b5f47d47a3f385dd72
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-03-13 10:58:23 +0000

    Modificado para considerar micrositios premier

--- swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/microsites/PromotionManager.java
+++ swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/microsites/PromotionManager.java
@@ -1,9 +1,29 @@
-
+/**
+* SemanticWebBuilder es una plataforma para el desarrollo de portales y aplicaciones de integración,
+* colaboración y conocimiento, que gracias al uso de tecnología semántica puede generar contextos de
+* información alrededor de algún tema de interés o bien integrar información y aplicaciones de diferentes
+* fuentes, donde a la información se le asigna un significado, de forma que pueda ser interpretada y
+* procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
+* para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
+*
+* INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+* en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
+* aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
+* todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
+* del SemanticWebBuilder 4.0.
+*
+* INFOTEC no otorga garantía sobre SemanticWebBuilder, de ninguna especie y naturaleza, ni implícita ni explícita,
+* siendo usted completamente responsable de la utilización que le dé y asumiendo la totalidad de los riesgos que puedan derivar
+* de la misma.
+*
+* Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
+* dirección electrónica:
+*  http://www.semanticwebbuilder.org
+**/
 package org.semanticwb.pymtur.microsites;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 import java.util.Iterator;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -17,17 +37,15 @@ import org.semanticwb.portal.api.GenericResource;
 import org.semanticwb.portal.api.SWBActionResponse;
 import org.semanticwb.portal.api.SWBParamRequest;
 import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.community.MicroSiteType;
 import org.semanticwb.pymtur.MicroSitePyme;
 import org.semanticwb.pymtur.Promotion;
-import org.semanticwb.pymtur.PromotionType;
 import org.semanticwb.pymtur.ServiceProvider;
 
 /**
  *
  * @author carlos.ramos
  */
-
-
 public class PromotionManager extends GenericResource {
     private static Logger log = SWBUtils.getLogger(PromotionManager.class);
 
@@ -65,7 +83,25 @@ public class PromotionManager extends GenericResource {
             }
             out.flush();
         }else {
-            RequestDispatcher dis = request.getRequestDispatcher("/work/models/etour/jsp/pymestur/microsite/spPromotions.jsp");
+            WebPage wp = paramRequest.getWebPage();
+            WebPage community = null;
+            String path = "";
+
+            if (wp instanceof MicroSitePyme) {
+                community = wp;
+            } else {
+                community = wp.getParent();
+            }
+
+            String siteUri = ((MicroSitePyme) community).getType().getURI();
+
+            if (MicroSiteType.ClassMgr.getMicroSiteType("MiPymeSite", wp.getWebSite()).getURI().equals(siteUri)) {
+                path = "/work/models/etour/jsp/pymestur/microsite/spPromotions.jsp";
+            } else if (MicroSiteType.ClassMgr.getMicroSiteType("MiPymeSitePlus", wp.getWebSite()).getURI().equals(siteUri)) {
+                path = "/work/models/etour/jsp/pymestur/premier/spPromotions.jsp";
+            }
+
+            RequestDispatcher dis = request.getRequestDispatcher(path);
             try {
                 request.setAttribute("paramRequest", paramRequest);
                 dis.include(request, response);
