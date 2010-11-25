13b9021e175454267270793d5ce25ae32111930d
redentor <redentor@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-11-25 19:04:00 +0000

    Adición de bloque de código estático para que el parser de estos objetos quede registrado y sea utilizado por el buscador.

--- swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/MicroSitePyme.java
+++ swbproys/pymestur/PYMTModel/src/org/semanticwb/pymtur/MicroSitePyme.java
@@ -1,8 +1,16 @@
 package org.semanticwb.pymtur;
 
+import org.semanticwb.SWBPortal;
+
+
 
 public class MicroSitePyme extends org.semanticwb.pymtur.base.MicroSitePymeBase 
 {
+
+    static {
+        SWBPortal.getIndexMgr().getDefaultIndexer().registerParser(MicroSitePyme.class, new MicroSiteParser());
+    }
+
     public MicroSitePyme(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
