e09cae6ee33f20369842ae71d8c5143c4ebb8c98
josesaid <josesaid@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-12-13 21:51:02 +0000

    cambios hechos por said

---
+++ swbproys/EcoSikan/src/org/semanticwb/ecosikan/widget/NewestIdeasByThemesAndChallengesResource.java
@@ -0,0 +1,37 @@
+package org.semanticwb.ecosikan.widget;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.model.WebPage;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+/**
+ *
+ * @author José Said Olano García
+ */
+public class NewestIdeasByThemesAndChallengesResource extends GenericResource{
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Recurso NewestIdeasByThemesAndChallengesResource  ");
+        final WebPage wp = paramRequest.getWebPage();
+        final String modelId = wp.getWebSiteId();
+        String path = "/work/models/"+modelId+"/jsp/widgets/las5IdeasMasNuevasPorTemaYPorRetos.jsp";
+        //RequestDispatcher rd = request.getRequestDispatcher("/work/said/las5IdeasMasNuevasPorTemaYPorRetos.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher(path);
+        try {
+            request.setAttribute("resource", this.getResourceBase());
+            request.setAttribute("paramRequest", paramRequest);
+            rd.include(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
