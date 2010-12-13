91fb24520e1f4625ea6a6ab9a466d7675146bcfc
josesaid <josesaid@60c509d2-c84e-0410-9847-e8532c4ce32d>
2010-12-13 22:02:24 +0000

    cambios hechos por said

---
+++ swbproys/EcoSikan/src/org/semanticwb/ecosikan/votes/borrame2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.ecosikan.votes;
+
+/**
+ *
+ * @author Administrator
+ */
+public class borrame2 {
+
+}
