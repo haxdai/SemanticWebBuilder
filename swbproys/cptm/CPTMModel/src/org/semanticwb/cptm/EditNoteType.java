8c055d9707e93a81b005c78a0abbf00824b21724
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-01-18 18:16:47 +0000

    CPTM v2

---
+++ swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/EditNoteType.java
@@ -0,0 +1,13 @@
+package org.semanticwb.cptm;
+
+
+   /**
+   * Catalogo de Tipos de Notas 
+   */
+public class EditNoteType extends org.semanticwb.cptm.base.EditNoteTypeBase 
+{
+    public EditNoteType(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
