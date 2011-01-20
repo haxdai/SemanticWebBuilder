03040430cf0f718421ee0c366316ad09b9891601
josesaid <josesaid@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-01-20 18:51:07 +0000

    tema modificado

--- swbproys/EcoSikan/src/org/semanticwb/ecosikan/innova/Comment.java
+++ swbproys/EcoSikan/src/org/semanticwb/ecosikan/innova/Comment.java
@@ -1,8 +1,5 @@
 package org.semanticwb.ecosikan.innova;
 
-import java.util.Comparator;
-
-
 public class Comment extends org.semanticwb.ecosikan.innova.base.CommentBase 
 {
     public Comment(org.semanticwb.platform.SemanticObject base)
