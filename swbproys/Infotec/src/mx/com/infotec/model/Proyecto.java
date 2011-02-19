78f40f4e7105bd6b2e90ea251a781e374797bb8c
jafernandez <jafernandez@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-02-19 00:19:54 +0000

    Se agregó la parte de proyectos

---
+++ swbproys/Infotec/src/mx/com/infotec/model/Proyecto.java
@@ -0,0 +1,10 @@
+package mx.com.infotec.model;
+
+
+public class Proyecto extends mx.com.infotec.model.base.ProyectoBase 
+{
+    public Proyecto(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
