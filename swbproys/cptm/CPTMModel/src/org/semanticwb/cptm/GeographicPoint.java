b5e1a2f0180a354898029b71fe5b8a88abc6f0db
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-02-22 22:19:08 +0000

    CPTM v2

--- swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/GeographicPoint.java
+++ swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/GeographicPoint.java
@@ -5,36 +5,47 @@ import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.platform.SemanticObserver;
 import org.semanticwb.platform.SemanticProperty;
 
+/**
+ * Clase que hereda de WebPage.Mediante estas se administra el catálogo de Destinos.
+ */
+public class GeographicPoint extends org.semanticwb.cptm.base.GeographicPointBase {
 
-   /**
-   * Clase que hereda de WebPage.Mediante estas se administra el catálogo de Destinos. 
-   */
-public class GeographicPoint extends org.semanticwb.cptm.base.GeographicPointBase 
-{
-    public GeographicPoint(org.semanticwb.platform.SemanticObject base)
-    {
-        super(base);
-        /*
-        LocalityInt.cptm_hasProximity.registerObserver(new SemanticObserver(){
-         public void notify(SemanticObject obj, Object prop, String lang, String action)
-            {
-                SemanticProperty semProp=(SemanticProperty)prop;
-                System.out.println("semProp:"+semProp.getName());
-                Iterator <SemanticObject> semObject=obj.listObjectProperties(semProp);
-                while(semObject.hasNext()){
-                    SemanticObject semObj=(SemanticObject)semObject.next();
-                    LocalityInt locality=(LocalityInt)semObj.createGenericInstance();
-                    if(!GeographicPoint.this.hasProximity(locality)){
-                        System.out.println("Agrega a:" +GeographicPoint.this+", la proximidad:"+locality);
-                        GeographicPoint.this.addProximity(locality);
+    static {
+        LocalityInt.cptm_hasProximity.registerObserver(new SemanticObserver() {
+            public void notify(SemanticObject obj, Object prop, String lang, String action) {
+                if(action!=null && obj.instanceOf(LocalityInt.cptm_LocalityInt))
+                {
+                    LocalityInt currentlpoint = (LocalityInt) obj.createGenericInstance();
+                    SemanticProperty semProp = (SemanticProperty) prop;
+                    if(action.equalsIgnoreCase("REMOVE")){
+                        Iterator<SemanticObject> semObject = obj.listObjectProperties(semProp);
+                        while (semObject.hasNext()) {
+                            SemanticObject semObj = (SemanticObject) semObject.next();
+                            LocalityInt locality = (LocalityInt) semObj.createGenericInstance();
+                            if (locality.hasProximity(currentlpoint)) {
+                                //System.out.println("Elimina de:" + locality + ", la proximidad inversa:" + currentlpoint);
+                                locality.getSemanticObject().removeObjectProperty(cptm_hasProximity, currentlpoint.getSemanticObject(), false);
+                            }
+                        }  
+                    }else if(action.equalsIgnoreCase("ADD")){                
+                            Iterator<SemanticObject> semObject = obj.listObjectProperties(semProp);
+                            while (semObject.hasNext()) {
+                                SemanticObject semObj = (SemanticObject) semObject.next();
+                                LocalityInt locality = (LocalityInt) semObj.createGenericInstance();
+                                if (currentlpoint.getURI() != locality.getURI() && !locality.hasProximity(currentlpoint)) {
+                                    //System.out.println("Agrega a:" + locality + ", la proximidad inversa:" + currentlpoint);
+                                    locality.getSemanticObject().addObjectProperty(cptm_hasProximity, currentlpoint.getSemanticObject(), false);
+                                }
+                            }    
                     }
                 }
-         }
+
+            }
         });
-         * */
-         
     }
 
+    public GeographicPoint(org.semanticwb.platform.SemanticObject base) {
+        super(base);        
+    }
     
-
 }
