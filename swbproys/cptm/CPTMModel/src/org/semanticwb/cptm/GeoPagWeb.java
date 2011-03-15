d081a88a9921e75c82f6d2afe7ee2b5d72622092
george24 <jajs1975@users.noreply.github.com>
2011-03-15 17:31:15 +0000

    CPTM v2

--- swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/GeoPagWeb.java
+++
@@ -1,8 +0,0 @@
-package org.semanticwb.cptm;
-
-   /**
-   * Interface para que solo se puedan agregar Puntos Geograficos y Paginas web en un Estado 
-   */
-public interface GeoPagWeb extends org.semanticwb.cptm.base.GeoPagWebBase
-{
-}
