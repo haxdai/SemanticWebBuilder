fa93add2c7deaa67551cf3487b3fbad2fa79026c
jafernandez <jafernandez@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-04-05 19:08:24 +0000

    Proyecto para la generación de catálogos

---
+++ swbproys/Catalogs/src/org/semanticwb/catalogs/CP.java
@@ -0,0 +1,10 @@
+package org.semanticwb.catalogs;
+
+
+public class CP extends org.semanticwb.catalogs.base.CPBase 
+{
+    public CP(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/Catalogs/src/org/semanticwb/catalogs/Estado.java
@@ -0,0 +1,10 @@
+package org.semanticwb.catalogs;
+
+
+public class Estado extends org.semanticwb.catalogs.base.EstadoBase 
+{
+    public Estado(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
