6aaadae580bffe2095f9f97505454409171cf68b
Juan Fernandez <jafdeza@users.noreply.github.com>
2011-04-05 19:08:24 +0000

    Proyecto para la generación de catálogos

---
+++ swbproys/Catalogs/src/org/semanticwb/catalogs/Municipio.java
@@ -0,0 +1,10 @@
+package org.semanticwb.catalogs;
+
+
+public class Municipio extends org.semanticwb.catalogs.base.MunicipioBase 
+{
+    public Municipio(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
