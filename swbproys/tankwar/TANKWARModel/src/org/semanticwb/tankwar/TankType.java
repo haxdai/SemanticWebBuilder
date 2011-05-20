46eb177d737666a61cd0281bbc2eab60a4423869
george24 <jajs1975@users.noreply.github.com>
2011-05-20 18:48:30 +0000

    ADS Project

---
+++ swbproys/tankwar/TANKWARModel/src/org/semanticwb/tankwar/TankType.java
@@ -0,0 +1,13 @@
+package org.semanticwb.tankwar;
+
+
+   /**
+   * Tipo de Tanque 
+   */
+public class TankType extends org.semanticwb.tankwar.base.TankTypeBase 
+{
+    public TankType(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/tankwar/TANKWARModel/src/org/semanticwb/tankwar/UserLike.java
@@ -0,0 +1,13 @@
+package org.semanticwb.tankwar;
+
+
+   /**
+   * Almacena los gustos del usuario. 
+   */
+public class UserLike extends org.semanticwb.tankwar.base.UserLikeBase 
+{
+    public UserLike(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
