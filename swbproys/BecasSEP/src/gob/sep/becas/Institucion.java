d204e5924c2683099de260b20ee4de10d2333a0d
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2011-07-29 16:49:58 +0000

    BecasSep Base

---
+++ swbproys/BecasSEP/src/gob/sep/becas/Institucion.java
@@ -0,0 +1,10 @@
+package gob.sep.becas;
+
+
+public class Institucion extends gob.sep.becas.base.InstitucionBase 
+{
+    public Institucion(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
