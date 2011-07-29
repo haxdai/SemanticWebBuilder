093d874d38eae5ba58f8fc35a40f9a2433f5353d
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-07-29 16:49:58 +0000

    BecasSep Base

---
+++ swbproys/BecasSEP/src/gob/sep/becas/NivelEstudio.java
@@ -0,0 +1,10 @@
+package gob.sep.becas;
+
+
+public class NivelEstudio extends gob.sep.becas.base.NivelEstudioBase 
+{
+    public NivelEstudio(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
