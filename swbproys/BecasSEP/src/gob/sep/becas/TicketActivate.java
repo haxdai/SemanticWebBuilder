cbe8d84d5bf7562fd7e93925cd7afaed00336616
serch <serch@60c509d2-c84e-0410-9847-e8532c4ce32d>
2011-08-03 00:01:48 +0000

    second pre-release

---
+++ swbproys/BecasSEP/src/gob/sep/becas/TicketActivate.java
@@ -0,0 +1,10 @@
+package gob.sep.becas;
+
+
+public class TicketActivate extends gob.sep.becas.base.TicketActivateBase 
+{
+    public TicketActivate(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
