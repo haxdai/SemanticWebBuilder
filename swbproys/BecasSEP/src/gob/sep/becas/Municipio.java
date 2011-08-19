8e260ff973513b57eca452d730008b4f29c11b08
Sergio Martínez <SergioMartinezP@users.noreply.github.com>
2011-08-19 16:04:32 +0000

    Added Estado & Municipio

---
+++ swbproys/BecasSEP/src/gob/sep/becas/Municipio.java
@@ -0,0 +1,10 @@
+package gob.sep.becas;
+
+
+public class Municipio extends gob.sep.becas.base.MunicipioBase 
+{
+    public Municipio(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
