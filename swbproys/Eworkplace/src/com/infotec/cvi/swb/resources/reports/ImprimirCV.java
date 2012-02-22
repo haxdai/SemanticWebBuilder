1f26b075571853dcf6c6d8282c67bae6ad1933fd
gsixtos <gsixtos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-02-22 18:27:50 +0000

    Con mucho comentado e impresiones a consola primer version para pruebas

--- swbproys/Eworkplace/src/com/infotec/cvi/swb/resources/reports/ImprimirCV.java
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/resources/reports/ImprimirCV.java
@@ -4,10 +4,38 @@
  */
 package com.infotec.cvi.swb.resources.reports;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.User;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
 /**
  *
- * @author juan.fernandez
+ * @author rene.jara
  */
-public class ImprimirCV {
-    
+public class ImprimirCV extends GenericResource {
+
+    private static Logger log = SWBUtils.getLogger(ImprimirCV.class);
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramsRequest) throws SWBResourceException, IOException {
+        PrintWriter out = response.getWriter();
+        String basePath = "/work/models/" + paramsRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        User user = paramsRequest.getUser();
+
+        RequestDispatcher dis = request.getRequestDispatcher(basePath + "/viewCV.jsp");
+        try {
+            request.setAttribute("paramRequest", paramsRequest);
+            dis.include(request, response);
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
 }
