966c7849f090f131fd63ffe9c9323814665149ce
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-03-06 18:17:41 +0000

    Definidas propiedades

--- swbproys/Eworkplace/src/com/infotec/eworkplace/swb/base/VacacionesBase.java
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/base/VacacionesBase.java
@@ -1,8 +1,14 @@
 package com.infotec.eworkplace.swb.base;
 
 
-public abstract class VacacionesBase extends org.semanticwb.model.SWBClass 
+public abstract class VacacionesBase extends org.semanticwb.model.SWBClass implements com.infotec.eworkplace.swb.Solicitable
 {
+    public static final org.semanticwb.platform.SemanticProperty intranet_fechaInicioVacaciones=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#fechaInicioVacaciones");
+    public static final org.semanticwb.platform.SemanticProperty intranet_periodo=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#periodo");
+    public static final org.semanticwb.platform.SemanticProperty intranet_nombreTitular=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#nombreTitular");
+    public static final org.semanticwb.platform.SemanticProperty intranet_fechaRegreso=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#fechaRegreso");
+    public static final org.semanticwb.platform.SemanticProperty intranet_nombreJefe=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#nombreJefe");
+    public static final org.semanticwb.platform.SemanticProperty intranet_fechaFinVacaciones=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticProperty("http://www.infotec.com.mx/intranet#fechaFinVacaciones");
     public static final org.semanticwb.platform.SemanticClass intranet_Vacaciones=org.semanticwb.SWBPlatform.getSemanticMgr().getVocabulary().getSemanticClass("http://www.infotec.com.mx/intranet#Vacaciones");
    /**
    * The semantic class that represents the currentObject
@@ -72,6 +78,29 @@ public abstract class VacacionesBase extends org.semanticwb.model.SWBClass
         {
             return (getVacaciones(id, model)!=null);
         }
+       /**
+       * Gets all com.infotec.eworkplace.swb.Vacaciones with a determined Solicitante
+       * @param value Solicitante of the type org.semanticwb.model.User
+       * @param model Model of the com.infotec.eworkplace.swb.Vacaciones
+       * @return Iterator with all the com.infotec.eworkplace.swb.Vacaciones
+       */
+
+        public static java.util.Iterator<com.infotec.eworkplace.swb.Vacaciones> listVacacionesBySolicitante(org.semanticwb.model.User value,org.semanticwb.model.SWBModel model)
+        {
+            org.semanticwb.model.GenericIterator<com.infotec.eworkplace.swb.Vacaciones> it=new org.semanticwb.model.GenericIterator(model.getSemanticObject().getModel().listSubjectsByClass(intranet_solicitante, value.getSemanticObject(),sclass));
+            return it;
+        }
+       /**
+       * Gets all com.infotec.eworkplace.swb.Vacaciones with a determined Solicitante
+       * @param value Solicitante of the type org.semanticwb.model.User
+       * @return Iterator with all the com.infotec.eworkplace.swb.Vacaciones
+       */
+
+        public static java.util.Iterator<com.infotec.eworkplace.swb.Vacaciones> listVacacionesBySolicitante(org.semanticwb.model.User value)
+        {
+            org.semanticwb.model.GenericIterator<com.infotec.eworkplace.swb.Vacaciones> it=new org.semanticwb.model.GenericIterator(value.getSemanticObject().getModel().listSubjectsByClass(intranet_solicitante,value.getSemanticObject(),sclass));
+            return it;
+        }
     }
 
    /**
@@ -81,5 +110,241 @@ public abstract class VacacionesBase extends org.semanticwb.model.SWBClass
     public VacacionesBase(org.semanticwb.platform.SemanticObject base)
     {
         super(base);
+    }
+   /**
+   * Sets the value for the property Solicitante
+   * @param value Solicitante to set
+   */
+
+    public void setSolicitante(org.semanticwb.model.User value)
+    {
+        if(value!=null)
+        {
+            getSemanticObject().setObjectProperty(intranet_solicitante, value.getSemanticObject());
+        }else
+        {
+            removeSolicitante();
+        }
+    }
+   /**
+   * Remove the value for Solicitante property
+   */
+
+    public void removeSolicitante()
+    {
+        getSemanticObject().removeProperty(intranet_solicitante);
+    }
+
+   /**
+   * Gets the Solicitante
+   * @return a org.semanticwb.model.User
+   */
+    public org.semanticwb.model.User getSolicitante()
+    {
+         org.semanticwb.model.User ret=null;
+         org.semanticwb.platform.SemanticObject obj=getSemanticObject().getObjectProperty(intranet_solicitante);
+         if(obj!=null)
+         {
+             ret=(org.semanticwb.model.User)obj.createGenericInstance();
+         }
+         return ret;
+    }
+
+/**
+* Gets the FechaInicioVacaciones property
+* @return java.util.Date with the FechaInicioVacaciones
+*/
+    public java.util.Date getFechaInicioVacaciones()
+    {
+        return getSemanticObject().getDateProperty(intranet_fechaInicioVacaciones);
+    }
+
+/**
+* Sets the FechaInicioVacaciones property
+* @param value long with the FechaInicioVacaciones
+*/
+    public void setFechaInicioVacaciones(java.util.Date value)
+    {
+        getSemanticObject().setDateProperty(intranet_fechaInicioVacaciones, value);
+    }
+
+/**
+* Gets the FechaSolicita property
+* @return java.util.Date with the FechaSolicita
+*/
+    public java.util.Date getFechaSolicita()
+    {
+        return getSemanticObject().getDateProperty(intranet_fechaSolicita);
+    }
+
+/**
+* Sets the FechaSolicita property
+* @param value long with the FechaSolicita
+*/
+    public void setFechaSolicita(java.util.Date value)
+    {
+        getSemanticObject().setDateProperty(intranet_fechaSolicita, value);
+    }
+
+/**
+* Gets the Periodo property
+* @return int with the Periodo
+*/
+    public int getPeriodo()
+    {
+        return getSemanticObject().getIntProperty(intranet_periodo);
+    }
+
+/**
+* Sets the Periodo property
+* @param value long with the Periodo
+*/
+    public void setPeriodo(int value)
+    {
+        getSemanticObject().setIntProperty(intranet_periodo, value);
+    }
+
+/**
+* Gets the NombreTitular property
+* @return String with the NombreTitular
+*/
+    public String getNombreTitular()
+    {
+        return getSemanticObject().getProperty(intranet_nombreTitular);
+    }
+
+/**
+* Sets the NombreTitular property
+* @param value long with the NombreTitular
+*/
+    public void setNombreTitular(String value)
+    {
+        getSemanticObject().setProperty(intranet_nombreTitular, value);
+    }
+
+/**
+* Gets the FechaRegreso property
+* @return java.util.Date with the FechaRegreso
+*/
+    public java.util.Date getFechaRegreso()
+    {
+        return getSemanticObject().getDateProperty(intranet_fechaRegreso);
+    }
+
+/**
+* Sets the FechaRegreso property
+* @param value long with the FechaRegreso
+*/
+    public void setFechaRegreso(java.util.Date value)
+    {
+        getSemanticObject().setDateProperty(intranet_fechaRegreso, value);
+    }
+
+/**
+* Gets the CargoSolicitante property
+* @return String with the CargoSolicitante
+*/
+    public String getCargoSolicitante()
+    {
+        return getSemanticObject().getProperty(intranet_cargoSolicitante);
+    }
+
+/**
+* Sets the CargoSolicitante property
+* @param value long with the CargoSolicitante
+*/
+    public void setCargoSolicitante(String value)
+    {
+        getSemanticObject().setProperty(intranet_cargoSolicitante, value);
+    }
+
+/**
+* Gets the AdscripcionSolicitante property
+* @return String with the AdscripcionSolicitante
+*/
+    public String getAdscripcionSolicitante()
+    {
+        return getSemanticObject().getProperty(intranet_adscripcionSolicitante);
+    }
+
+/**
+* Sets the AdscripcionSolicitante property
+* @param value long with the AdscripcionSolicitante
+*/
+    public void setAdscripcionSolicitante(String value)
+    {
+        getSemanticObject().setProperty(intranet_adscripcionSolicitante, value);
+    }
+
+/**
+* Gets the NombreJefe property
+* @return String with the NombreJefe
+*/
+    public String getNombreJefe()
+    {
+        return getSemanticObject().getProperty(intranet_nombreJefe);
+    }
+
+/**
+* Sets the NombreJefe property
+* @param value long with the NombreJefe
+*/
+    public void setNombreJefe(String value)
+    {
+        getSemanticObject().setProperty(intranet_nombreJefe, value);
+    }
+
+/**
+* Gets the ExtensionSolicitante property
+* @return String with the ExtensionSolicitante
+*/
+    public String getExtensionSolicitante()
+    {
+        return getSemanticObject().getProperty(intranet_extensionSolicitante);
+    }
+
+/**
+* Sets the ExtensionSolicitante property
+* @param value long with the ExtensionSolicitante
+*/
+    public void setExtensionSolicitante(String value)
+    {
+        getSemanticObject().setProperty(intranet_extensionSolicitante, value);
+    }
+
+/**
+* Gets the NombreSolicitante property
+* @return String with the NombreSolicitante
+*/
+    public String getNombreSolicitante()
+    {
+        return getSemanticObject().getProperty(intranet_nombreSolicitante);
+    }
+
+/**
+* Sets the NombreSolicitante property
+* @param value long with the NombreSolicitante
+*/
+    public void setNombreSolicitante(String value)
+    {
+        getSemanticObject().setProperty(intranet_nombreSolicitante, value);
+    }
+
+/**
+* Gets the FechaFinVacaciones property
+* @return java.util.Date with the FechaFinVacaciones
+*/
+    public java.util.Date getFechaFinVacaciones()
+    {
+        return getSemanticObject().getDateProperty(intranet_fechaFinVacaciones);
+    }
+
+/**
+* Sets the FechaFinVacaciones property
+* @param value long with the FechaFinVacaciones
+*/
+    public void setFechaFinVacaciones(java.util.Date value)
+    {
+        getSemanticObject().setDateProperty(intranet_fechaFinVacaciones, value);
     }
 }
