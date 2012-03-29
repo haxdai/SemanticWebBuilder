5da8fa0ae4eef6db3bee8e309bd3b2fa6fd34c24
george24 <george24@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-03-29 17:57:38 +0000

    Arregle un patron para la páginación, ya que no funcionaba con Crome

---
+++ swbproys/ssmcc/SOCIALMODEL/src/org/swb/social/model/SocialNetwork.java
@@ -0,0 +1,13 @@
+package org.swb.social.model;
+
+
+   /**
+   * Objeto que se encarga de almacenar todos los datos de una red social 
+   */
+public class SocialNetwork extends org.swb.social.model.base.SocialNetworkBase 
+{
+    public SocialNetwork(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/ssmcc/SOCIALMODEL/src/org/swb/social/model/SocialUser.java
@@ -0,0 +1,13 @@
+package org.swb.social.model;
+
+
+   /**
+   * Usuario que ha escrito un post en una red social 
+   */
+public class SocialUser extends org.swb.social.model.base.SocialUserBase 
+{
+    public SocialUser(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
