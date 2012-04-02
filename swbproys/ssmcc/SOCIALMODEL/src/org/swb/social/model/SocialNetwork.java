93f822887542f8b0b4b50bd844fcd83fa67bec9c
george24 <jajs1975@users.noreply.github.com>
2012-04-02 18:15:40 +0000

    SWBSocial

--- swbproys/ssmcc/SOCIALMODEL/src/org/swb/social/model/SocialNetwork.java
+++ swbproys/ssmcc/SOCIALMODEL/src/org/swb/social/model/SocialNetwork.java
@@ -1,12 +1,13 @@
 package org.swb.social.model;
 
+import org.swb.social.lib.Exception.SWBSocialNetworkException;
 import org.swb.social.lib.SocialNetWorkInt;
 
 
    /**
    * Objeto que se encarga de almacenar todos los datos de una red social 
    */
-public abstract class SocialNetwork extends org.swb.social.model.base.SocialNetworkBase implements SocialNetWorkInt
+public class SocialNetwork extends org.swb.social.model.base.SocialNetworkBase implements SocialNetWorkInt
 {
     public String msg=null;
     String photo=null;
@@ -17,7 +18,44 @@ public abstract class SocialNetwork extends org.swb.social.model.base.SocialNetw
         super(base);
     }
 
-    
+    //Sets
+    public void setMsg(String msg) {
+        this.msg=msg;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo=photo;
+    }
+
+    public void setVideo(String video) {
+        this.video=video;
+    }
+
+    //Gets
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getPhoto() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getVideo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void sendPost() throws SWBSocialNetworkException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void connect() throws SWBSocialNetworkException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void disConnect() throws SWBSocialNetworkException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 
 }
