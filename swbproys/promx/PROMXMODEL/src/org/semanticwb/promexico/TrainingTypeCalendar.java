e2bb8c99178d5e03ac28085f92670fb7e4a92b6b
martha <martha@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-04-16 16:32:23 +0000

    Nuevo Requerimientos de ProMX

---
+++ swbproys/promx/PROMXMODEL/src/org/semanticwb/promexico/TrainingTypeCalendar.java
@@ -0,0 +1,10 @@
+package org.semanticwb.promexico;
+
+
+public class TrainingTypeCalendar extends org.semanticwb.promexico.base.TrainingTypeCalendarBase 
+{
+    public TrainingTypeCalendar(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
