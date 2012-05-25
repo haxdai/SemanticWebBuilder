9015885bebb3281ab74952a0d05236e8fc116216
redentor <JRedentor@users.noreply.github.com>
2012-05-25 21:33:57 +0000

    Adición de la propiedad utilizada para el ordenamiento de los datos en el despliegue.

---
+++ swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/resources/version2012/Location.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.semanticwb.cptm.resources.version2012;
+
+/**
+ *
+ * @author jose.jimenez
+ */
+public class Location {
+
+}
