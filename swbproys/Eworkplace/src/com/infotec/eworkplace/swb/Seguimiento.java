1d15794ce3d365debeb36c068eefef171f80b1c7
Hasdai Pacheco <me@hasdaipacheco.com>
2012-07-24 22:30:31 +0000

    Creado

---
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/Seguimiento.java
@@ -0,0 +1,10 @@
+package com.infotec.eworkplace.swb;
+
+
+public class Seguimiento extends com.infotec.eworkplace.swb.base.SeguimientoBase 
+{
+    public Seguimiento(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
