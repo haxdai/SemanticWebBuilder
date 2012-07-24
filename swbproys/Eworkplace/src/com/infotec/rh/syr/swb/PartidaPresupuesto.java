2ac27d2607c7c03c735db75c3dc5081ababf084b
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-07-24 22:29:38 +0000

    Generado código de objetos para proceso STA

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/PartidaPresupuesto.java
@@ -0,0 +1,13 @@
+package com.infotec.rh.syr.swb;
+
+
+   /**
+   * Encapsula las propiedades correspondientes a una aprtida presupuestal 
+   */
+public class PartidaPresupuesto extends com.infotec.rh.syr.swb.base.PartidaPresupuestoBase 
+{
+    public PartidaPresupuesto(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SeguimientoSolicitudRecurso.java
@@ -0,0 +1,13 @@
+package com.infotec.rh.syr.swb;
+
+
+   /**
+   * Encapsula las propiedades de seguimiento asociadas a una solicitud de recursos humanos 
+   */
+public class SeguimientoSolicitudRecurso extends com.infotec.rh.syr.swb.base.SeguimientoSolicitudRecursoBase 
+{
+    public SeguimientoSolicitudRecurso(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
