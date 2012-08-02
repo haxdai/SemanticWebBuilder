eb7217aad649a25e15dceb6cf296fada8f38213c
Hasdai Pacheco <me@hasdaipacheco.com>
2012-08-02 22:16:50 +0000

    Creados para los servicios de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SolicitudBajaRecurso.java
@@ -0,0 +1,10 @@
+package com.infotec.rh.syr.swb;
+
+
+public class SolicitudBajaRecurso extends com.infotec.rh.syr.swb.base.SolicitudBajaRecursoBase 
+{
+    public SolicitudBajaRecurso(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
