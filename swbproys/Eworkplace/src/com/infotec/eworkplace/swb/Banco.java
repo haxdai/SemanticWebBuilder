e32de7cfaab754e6cca32caf5adac1c61228bef3
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-02 23:19:26 +0000

    Creado para administrar instituciones bancarias en servicios de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/Banco.java
@@ -0,0 +1,10 @@
+package com.infotec.eworkplace.swb;
+
+
+public class Banco extends com.infotec.eworkplace.swb.base.BancoBase 
+{
+    public Banco(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
