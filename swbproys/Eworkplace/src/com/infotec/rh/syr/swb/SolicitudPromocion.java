006c0a0e961a7e41da297fba8fec2e581d2a059a
Hasdai Pacheco <me@hasdaipacheco.com>
2012-08-02 20:58:23 +0000

    Creado objeto para servicios de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SolicitudPromocion.java
@@ -0,0 +1,10 @@
+package com.infotec.rh.syr.swb;
+
+
+public class SolicitudPromocion extends com.infotec.rh.syr.swb.base.SolicitudPromocionBase 
+{
+    public SolicitudPromocion(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
