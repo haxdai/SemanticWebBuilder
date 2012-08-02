3a5eda47ac82095c7e6b5d9edd8f537e9a3cdf1c
Hasdai Pacheco <me@hasdaipacheco.com>
2012-08-02 23:16:41 +0000

    Agregado para servicios de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SeguimientoCandidato.java
@@ -0,0 +1,10 @@
+package com.infotec.rh.syr.swb;
+
+
+public class SeguimientoCandidato extends com.infotec.rh.syr.swb.base.SeguimientoCandidatoBase 
+{
+    public SeguimientoCandidato(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
