c1f8f58530dfaa75142aa24fe94e832103753df0
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-08 15:23:29 +0000

    Creado para los procesos de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/ActividadesArea.java
@@ -0,0 +1,13 @@
+package com.infotec.cvi.swb;
+
+
+   /**
+   * Adminsitra las actividades para CCO; DCP, ACD de RH 
+   */
+public class ActividadesArea extends com.infotec.cvi.swb.base.ActividadesAreaBase 
+{
+    public ActividadesArea(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
