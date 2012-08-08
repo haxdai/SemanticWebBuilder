33927a856c035991ecd4df6c5243214b29af0f1f
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-08 15:24:29 +0000

    Creado para los procesos de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SeguimientoActCCO.java
@@ -0,0 +1,13 @@
+package com.infotec.rh.syr.swb;
+
+
+   /**
+   * Encapsula las propiedades de seguimiento al proceso CCO de RH 
+   */
+public class SeguimientoActCCO extends com.infotec.rh.syr.swb.base.SeguimientoActCCOBase 
+{
+    public SeguimientoActCCO(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
