f55639673682ed09cef4c8657511bdf004943e59
Hasdai Pacheco <me@hasdaipacheco.com>
2012-08-08 15:24:29 +0000

    Creado para los procesos de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SeguimientoActACD.java
@@ -0,0 +1,13 @@
+package com.infotec.rh.syr.swb;
+
+
+   /**
+   * Encapsula las propiedades de seguimiento al proceso ACD de RH 
+   */
+public class SeguimientoActACD extends com.infotec.rh.syr.swb.base.SeguimientoActACDBase 
+{
+    public SeguimientoActACD(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/Eworkplace/src/com/infotec/rh/syr/swb/SeguimientoActDCP.java
@@ -0,0 +1,13 @@
+package com.infotec.rh.syr.swb;
+
+
+   /**
+   * Encapsula las propiedades de seguimiento al proceso DCP de RH 
+   */
+public class SeguimientoActDCP extends com.infotec.rh.syr.swb.base.SeguimientoActDCPBase 
+{
+    public SeguimientoActDCP(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
