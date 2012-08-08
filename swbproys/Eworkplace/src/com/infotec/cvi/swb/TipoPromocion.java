c48a78a0962c3c823a22e937ee6b155ed5a3f051
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-08 15:23:05 +0000

    Creado para los procesos de Recursos Humanos

---
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/TipoPromocion.java
@@ -0,0 +1,13 @@
+package com.infotec.cvi.swb;
+
+
+   /**
+   * Catálogo utilizado para indicar el Tipo de promoción 
+   */
+public class TipoPromocion extends com.infotec.cvi.swb.base.TipoPromocionBase 
+{
+    public TipoPromocion(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
