384e48ad52c44c26af33db261ed978aeeefff2f9
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-08-27 23:12:59 +0000

    código regenerado

--- swbproys/Eworkplace/src/com/infotec/cvi/swb/base/EstudiosBase.java
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/base/EstudiosBase.java
@@ -4,7 +4,7 @@ package com.infotec.cvi.swb.base;
    /**
    * Catálogo utilizado para almacenar los diferentes Estudios Superiores 
    */
-public abstract class EstudiosBase extends com.infotec.cvi.swb.Catalogo implements org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable
+public abstract class EstudiosBase extends com.infotec.cvi.swb.Catalogo implements org.semanticwb.model.Descriptiveable,org.semanticwb.model.Traceable
 {
    /**
    * Catálogo utilizado para las areas que agrupan a las carreras de los Estudios Superiores
