f026017c71dabd2adfbc681d9de59240e0bd5c7e
Juan Fernandez <jafdeza@users.noreply.github.com>
2012-10-23 19:21:04 +0000

    Regeneración de código base

---
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/SedeRecurso.java
@@ -0,0 +1,10 @@
+package com.infotec.eworkplace.swb;
+
+
+public class SedeRecurso extends com.infotec.eworkplace.swb.base.SedeRecursoBase 
+{
+    public SedeRecurso(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
