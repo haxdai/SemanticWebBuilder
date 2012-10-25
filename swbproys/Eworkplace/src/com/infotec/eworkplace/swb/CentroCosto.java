1253b294079aefdeca863e037926aaac51addf95
Juan Fernandez <jafdeza@users.noreply.github.com>
2012-10-25 22:15:07 +0000

    Regeneración de código base

---
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/CentroCosto.java
@@ -0,0 +1,10 @@
+package com.infotec.eworkplace.swb;
+
+
+public class CentroCosto extends com.infotec.eworkplace.swb.base.CentroCostoBase 
+{
+    public CentroCosto(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
---
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/EspecialidadSolicitudRecurso.java
@@ -0,0 +1,10 @@
+package com.infotec.eworkplace.swb;
+
+
+public class EspecialidadSolicitudRecurso extends com.infotec.eworkplace.swb.base.EspecialidadSolicitudRecursoBase 
+{
+    public EspecialidadSolicitudRecurso(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
