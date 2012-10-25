92d40864d6ec4bcc6be0fc872b83d6db72aa8ae6
Juan Fernandez <jafdeza@users.noreply.github.com>
2012-10-25 22:14:41 +0000

    Regeneración de código base

--- swbproys/Eworkplace/src/com/infotec/cvi/swb/base/GradoBase.java
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/base/GradoBase.java
@@ -4,7 +4,7 @@ package com.infotec.cvi.swb.base;
    /**
    * Catálogo utilizado para indicar el Grado Académico alcanzado. 
    */
-public abstract class GradoBase extends com.infotec.cvi.swb.Catalogo implements org.semanticwb.model.Sortable,org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable
+public abstract class GradoBase extends com.infotec.cvi.swb.Catalogo implements org.semanticwb.model.Traceable,org.semanticwb.model.Descriptiveable,org.semanticwb.model.Sortable
 {
    /**
    * Catálogo utilizado para indicar el Grado Académico alcanzado.
