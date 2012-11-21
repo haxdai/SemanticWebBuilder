0a0b39dd9923efd77db917d80099144213fdbd7c
cramos <cramos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2012-11-21 22:31:59 +0000

    utileriías para reportes csv de rh

--- swbproys/Eworkplace/src/com/infotec/eworkplace/swb/Domicilio.java
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/Domicilio.java
@@ -7,4 +7,25 @@ public class Domicilio extends com.infotec.eworkplace.swb.base.DomicilioBase
     {
         super(base);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder d = new StringBuilder();
+        d.append(getCalle());
+        d.append(" ");
+        d.append(getNumExterior());
+        d.append(" ");
+        if(!getNumInterior().isEmpty()) {            
+            d.append("Int."+getNumInterior());
+            d.append(" ");
+        }
+        d.append(getCp());
+        d.append(" ");
+        d.append(getColonia());
+        d.append(" ");
+        d.append(getMunicipio()==null?"":getMunicipio().getTitle()+" ");        
+        d.append(getEntidad()==null?"":getEntidad().getTitle()+" ");
+        d.append(getPais()==null?"":getPais().getTitle()+" ");
+        return d.toString();
+    }
 }
