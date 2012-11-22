eec8d094660fc6193dd43202e9ac06373cb29332
cramos <ciramosi@users.noreply.github.com>
2012-11-22 17:27:06 +0000

    utileriías para reportes csv de rh

--- swbproys/Eworkplace/src/com/infotec/cvi/swb/Academia.java
+++ swbproys/Eworkplace/src/com/infotec/cvi/swb/Academia.java
@@ -12,6 +12,59 @@ public class Academia extends com.infotec.cvi.swb.base.AcademiaBase
     {
         super(base);
     }
+        
+    public String getDisciplina() {
+        String disciplina = "Sin estudios";
+                    
+        TreeMap<Integer, SWBClass> estudios = new TreeMap();
+        
+        Iterator itso = listEstudioSuperiors();
+        if(itso.hasNext())
+        {
+            while(itso.hasNext())
+            {
+                EstudioSuperior obj = (EstudioSuperior)itso.next();
+                TipoEstudio tipoEstudio = obj.getEstudiosSuperiores().getAreaEstudio().getDisciplinaInv().getTipoEstudioInv();
+                estudios.put(tipoEstudio.getIndex()+obj.getGradoAvance().getIndex(), obj);
+            }
+            SWBClass swbclass = estudios.get(estudios.lastKey());
+            EstudioSuperior obj = (EstudioSuperior)swbclass;
+            disciplina = obj.getEstudiosSuperiores().getAreaEstudio().getTitle()+" "+obj.getEstudiosSuperiores().getAreaEstudio().getDisciplinaInv().getTitle();
+            return disciplina;
+        }
+        
+        itso = listGradoAcademicos();
+        if( itso.hasNext() )
+        {
+            while(itso.hasNext())
+            {
+                GradoAcademico obj = (GradoAcademico)itso.next();
+                estudios.put(obj.getGrado().getIndex()+obj.getSituacionAcademica().getIndex(),obj);
+            }
+            SWBClass swbclass = estudios.get(estudios.lastKey());
+            GradoAcademico obj = (GradoAcademico)swbclass;
+            disciplina = obj.getCarrera().getAreaCarrera().getTitle();
+            return disciplina;
+        }
+        return disciplina;
+        
+//        try
+//        {
+//            SWBClass swbclass = estudios.get(estudios.lastKey());
+//            if( swbclass instanceof EstudioSuperior )
+//            {
+//                EstudioSuperior obj = (EstudioSuperior)swbclass;
+//                maxNivelEstudios = obj.getOtroEstudio()==null ? obj.getEstudiosSuperiores().getAreaEstudio().getTitle()+" "+obj.getEstudiosSuperiores().getTitle() : obj.getOtroEstudio(); 
+//            }else if( swbclass instanceof GradoAcademico )
+//            {
+//                GradoAcademico obj = (GradoAcademico)swbclass;
+//                maxNivelEstudios = obj.getCarrera().getTitle();
+//            }
+//        }catch(NoSuchElementException nse)
+//        {
+//        }catch(Exception e){}
+//        return maxNivelEstudios;
+    }
     
     public String getMaxNivelEstudios() {
         String maxNivelEstudios = "Sin estudios";
