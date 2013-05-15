77b68f181c936116108be46a09ea9a57c6c7ba7e
gsixtos <renejara-infotec@users.noreply.github.com>
2013-05-15 01:22:27 +0000

    version inicial

---
+++ swbproys/LODP/src/com/infotec/lodp/swb/resources/CommentsViewResource.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.infotec.lodp.swb.resources;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.model.Resource;
+import org.semanticwb.model.User;
+import org.semanticwb.model.UserRepository;
+import org.semanticwb.model.WebPage;
+import org.semanticwb.model.WebSite;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBActionResponse;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+/**
+ *
+ * @author rene.jara
+ */
+public class CommentsViewResource extends GenericResource  {
+    public static final Logger log = SWBUtils.getLogger(CommentsViewResource.class);
+
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        String mode = paramRequest.getMode();
+        super.processRequest(request, response, paramRequest);
+    }
+
+    @Override
+    public void processAction(HttpServletRequest request, SWBActionResponse response) throws SWBResourceException, IOException {
+        String action = response.getAction();
+        WebPage wpage = response.getWebPage();
+        WebSite wsite = wpage.getWebSite();
+        Resource base = getResourceBase();
+        User usr = response.getUser();
+        UserRepository ur = wsite.getUserRepository();
+
+        if(response.Action_ADD.equals(action)) {
+        }
+    }
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramsRequest) throws SWBResourceException, IOException {
+        String basePath = "/work/models/" + paramsRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        String path = basePath + "view.jsp";
+        if (request != null) {
+            RequestDispatcher dis = request.getRequestDispatcher(path);
+            if (null != dis) {
+                try {
+                    request.setAttribute("paramRequest", paramsRequest);
+                    request.setAttribute("this", this);
+                    dis.include(request, response);
+                } catch (Exception e) {
+                    log.error(e);
+                }
+            }
+        }
+    }
+}
