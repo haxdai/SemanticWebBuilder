f778ec97242afe6b9eb90d1ed9afe1f4a44cc241
gsixtos <gsixtos@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-05-27 18:24:11 +0000

    version inicial

---
+++ swbproys/LODP/src/com/infotec/lodp/swb/resources/MapMoreViewResource.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.infotec.lodp.swb.resources;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.Logger;
+import org.semanticwb.SWBUtils;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+/**
+ *
+ * @author rene.jara
+ */
+public class MapMoreViewResource extends GenericResource{
+
+    public static final Logger log = SWBUtils.getLogger(MapMoreViewResource.class);
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        String basePath = "/work/models/" + paramRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        String path = basePath + "view.jsp";
+        if (request != null) {
+            RequestDispatcher dis = request.getRequestDispatcher(path);
+            if (null != dis) {
+                try {
+                    request.setAttribute("paramRequest", paramRequest);
+                    request.setAttribute("this", this);
+                    dis.include(request, response);
+                } catch (Exception e) {
+                    log.error(e);
+                }
+            }
+        }
+    }
+
+}
