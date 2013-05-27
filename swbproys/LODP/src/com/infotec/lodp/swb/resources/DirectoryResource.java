1d03928b0b85913cdf2d06da3ee51deb99675702
lennin <lennin@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-05-27 21:19:33 +0000

    Se agrega recurso para directorio de publicadores

---
+++ swbproys/LODP/src/com/infotec/lodp/swb/resources/DirectoryResource.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infotec.lodp.swb.resources;
+
+import static com.infotec.lodp.swb.resources.DataSetResource.log;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.SWBParamRequest;
+import org.semanticwb.portal.api.SWBResourceException;
+
+/**
+ *
+ * @author Lennin
+ */
+public class DirectoryResource extends GenericResource{
+    
+    public static final String MODE_DETAIL = "DETAIL";
+
+    @Override
+    public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        System.out.println("Entrooooo metodo doView");
+        String basePath = "/work/models/" + paramRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        String path = basePath + "view.jsp";
+        if (request != null) {
+            RequestDispatcher dis = request.getRequestDispatcher(path);
+            if (null != dis) {
+                try {     
+                    request.setAttribute("paramRequest", paramRequest);
+                    request.setAttribute("this", this);
+                    dis.include(request, response);
+                } catch (Exception e) {
+                    log.error(e);
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void processRequest(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        
+        String mode = paramRequest.getMode();
+        
+            if(mode.equals(MODE_DETAIL)){
+                doViewDetail(request, response, paramRequest);
+            } else {
+                super.processRequest(request, response, paramRequest);
+            }
+        
+        
+    }
+    
+    public void doViewDetail(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, IOException {
+        System.out.println("Entrooooo metodo doView");
+        String basePath = "/work/models/" + paramRequest.getWebPage().getWebSite().getId() + "/jsp/" + this.getClass().getSimpleName() + "/";
+        String path = basePath + "detailPublisher.jsp";
+        if (request != null) {
+            RequestDispatcher dis = request.getRequestDispatcher(path);
+            if (null != dis) {
+                try {     
+                    request.setAttribute("paramRequest", paramRequest);
+                    request.setAttribute("this", this);
+                    dis.include(request, response);
+                } catch (Exception e) {
+                    log.error(e);
+                }
+            }
+        }
+    }
+    
+    
+    
+}
