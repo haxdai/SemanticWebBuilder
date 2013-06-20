cc699a44da9d06756cda828758ca7a66dfc2fa28
gabriela <gabriela@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-06-20 22:15:03 +0000

    Modificacion hasMultipleBankAnswer

---
+++ SWBSurveyMaturity/src/org/semanticwb/questionnaire/MultipleBankAnswer.java
@@ -0,0 +1,10 @@
+package org.semanticwb.questionnaire;
+
+
+public class MultipleBankAnswer extends org.semanticwb.questionnaire.base.MultipleBankAnswerBase 
+{
+    public MultipleBankAnswer(org.semanticwb.platform.SemanticObject base)
+    {
+        super(base);
+    }
+}
