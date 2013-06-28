afb3db8a98b2da6e2cb783199199791bd79e8140
Hasdai Pacheco <me@hasdaipacheco.com>
2013-06-28 17:58:54 +0000

    Agregados métodos para optimizar la recuperación de reservaciones. Modificados setFechaInicio y setFechaFin para establecer los intervalos en la propiedad hasFechaReservada

--- swbproys/Eworkplace/src/com/infotec/eworkplace/swb/ReservacionSala.java
+++ swbproys/Eworkplace/src/com/infotec/eworkplace/swb/ReservacionSala.java
@@ -1,9 +1,17 @@
 package com.infotec.eworkplace.swb;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 import org.semanticwb.SWBUtils;
 import org.semanticwb.model.SWBContext;
+import org.semanticwb.platform.SemanticModel;
+import org.semanticwb.platform.SemanticObject;
 
 
 public class ReservacionSala extends com.infotec.eworkplace.swb.base.ReservacionSalaBase 
@@ -60,5 +68,52 @@ public class ReservacionSala extends com.infotec.eworkplace.swb.base.Reservacion
         }catch(Exception e) {            
         }
         return sb.toString();
+    }
+    
+    public static List<ReservacionSala> getReservationsByDay(Date day, SemanticModel model) {
+        List<ReservacionSala> ret = new ArrayList<ReservacionSala>();
+        
+        String stamp = SWBUtils.TEXT.iso8601DateFormat(day);
+        Iterator<SemanticObject> itsubj = model.listSubjects(ReservacionSala.intranet_hasFechaReservada, stamp);
+        while (itsubj.hasNext()) {
+            SemanticObject sobj = itsubj.next();
+            if (sobj != null && sobj.instanceOf(ReservacionSala.sclass)) {
+                ret.add((ReservacionSala)sobj.createGenericInstance());
+            }
+        }
+        return ret;
+    }
+
+    @Override
+    public void setFechaInicio(Date value) {
+        super.setFechaInicio(value);
+        setReservedDates();
+    }
+
+    @Override
+    public void setFechaFinal(Date value) {
+        super.setFechaFinal(value);
+        setReservedDates();
+    }
+    
+    private void setReservedDates() {
+        if (getFechaInicio() != null && getFechaFinal() != null) {
+            removeAllFechaReservada();
+            Calendar _s = GregorianCalendar.getInstance();
+            Calendar _e = GregorianCalendar.getInstance();
+            _s.setTime(getFechaInicio());
+            _e.setTime(getFechaFinal());
+
+            _s.set(Calendar.HOUR_OF_DAY, 0);
+            _s.set(Calendar.MINUTE, 0);
+            _e.set(Calendar.HOUR_OF_DAY, 0);
+            _e.set(Calendar.MINUTE, 0);
+
+            addFechaReservada(SWBUtils.TEXT.iso8601DateFormat(_s.getTime()));
+            while (_s.compareTo(_e) < 0) {
+                _s.add(Calendar.DAY_OF_MONTH, 1);
+                addFechaReservada(SWBUtils.TEXT.iso8601DateFormat(_s.getTime()));
+            }
+        }
     }
 }
