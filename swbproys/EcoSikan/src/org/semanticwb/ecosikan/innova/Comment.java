dc2e60db04d98d1b95919d22a94c0ea73cf0fdb1
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-07-16 21:36:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /swbproys/EcoSikan/src/org/semanticwb/ecosikan/innova/Comment.java

--- swbproys/EcoSikan/src/org/semanticwb/ecosikan/innova/Comment.java
+++
@@ -1,9 +0,0 @@
-package org.semanticwb.ecosikan.innova;
-
-public class Comment extends org.semanticwb.ecosikan.innova.base.CommentBase 
-{
-    public Comment(org.semanticwb.platform.SemanticObject base)
-    {
-        super(base);
-    }
-}
