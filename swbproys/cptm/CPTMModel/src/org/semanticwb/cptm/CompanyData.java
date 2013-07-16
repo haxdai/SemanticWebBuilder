9147475b170ae67f085efb0a3c5632e324bc11ee
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-07-16 21:46:43 +0000

    Autoversioning commit:  a non-deltaV client made a change to /swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/CompanyData.java

--- swbproys/cptm/CPTMModel/src/org/semanticwb/cptm/CompanyData.java
+++
@@ -1,8 +0,0 @@
-package org.semanticwb.cptm;
-
-   /**
-   * Interface para datos generales de Cadenas, Marcas y Empresas 
-   */
-public interface CompanyData extends org.semanticwb.cptm.base.CompanyDataBase
-{
-}
