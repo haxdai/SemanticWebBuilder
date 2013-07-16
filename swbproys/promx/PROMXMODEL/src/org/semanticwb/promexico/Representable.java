1c6b5deea00468f2c7f5d3856436c0b2003be1ed
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2013-07-16 21:51:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /swbproys/promx/PROMXMODEL/src/org/semanticwb/promexico/Representable.java

--- swbproys/promx/PROMXMODEL/src/org/semanticwb/promexico/Representable.java
+++
@@ -1,8 +0,0 @@
-package org.semanticwb.promexico;
-
-   /**
-   * Datos de un representante, se utiliza para las oficinas y para las regiones 
-   */
-public interface Representable extends org.semanticwb.promexico.base.RepresentableBase
-{
-}
