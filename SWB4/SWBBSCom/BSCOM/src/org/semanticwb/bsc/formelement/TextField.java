823fbe92f441b9b04f88af27fb4cae25c9605379
Martha <marthajimenez@users.noreply.github.com>
2014-11-13 19:34:47 +0000

    Modificación al formElement que visualiza el titulo de un elemento BSCElement en las vistas resumen.

--- SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/TextField.java
+++ SWB4/SWBBSCom/BSCOM/src/org/semanticwb/bsc/formelement/TextField.java
@@ -8,14 +8,13 @@ import org.semanticwb.platform.SemanticClass;
 import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.platform.SemanticProperty;
 
-
-   /**
-   * Muestra en el modo vista, una liga hacia la página que contenga la información
-   * del tipo de objeto correspondiente, para bsc:Objective, bsc:Indicator, bsc:Initiative y bsc:Deliverable 
-   */
+/**
+ * Muestra en el modo vista, una liga hacia la página que contenga la
+ * información del tipo de objeto correspondiente, para bsc:Objective,
+ * bsc:Indicator, bsc:Initiative y bsc:Deliverable
+ */
 public class TextField extends org.semanticwb.bsc.formelement.base.TextFieldBase {
-    
-    
+
     public TextField(org.semanticwb.platform.SemanticObject base) {
         super(base);
     }
@@ -29,44 +28,39 @@ public class TextField extends org.semanticwb.bsc.formelement.base.TextFieldBase
     @Override
     public String renderElement(HttpServletRequest request, SemanticObject obj,
             SemanticProperty prop, String propName, String type, String mode, String lang) {
-        
+
         String toReturn = null;
         String websiteId = ((SWBModel) obj.getModel().getModelObject().getGenericInstance()).getId();
         //(String) request.getAttribute("websiteId"); se cambio por la linea anterior
-        
+
         if (mode == null) {
             mode = "view";
         }
         if (mode.equals("edit") || mode.equals("create")) {
             toReturn = super.renderElement(request, obj, prop, propName, type, mode, lang);
         } else if (mode.equals("view")) {
-            boolean showLink = false;
             StringBuilder viewString = new StringBuilder(128);
             String baseUrl = null;
-            
+
             GenericObject genObject = obj.createGenericInstance();
             SemanticClass rootClass = obj.getSemanticClass().getRootClass();
+            viewString.append(obj.getProperty(prop));
             if (rootClass.equals(BSCElement.bsc_BSCElement)) {
                 if (request.getAttribute("pdf") == null) {
-                showLink = true;
-                viewString.append("<a href=\"");
-                if (websiteId != null && request.getRequestURI().contains(websiteId)) {
-                    baseUrl = request.getRequestURI().substring(0,
-                            request.getRequestURI().indexOf(websiteId) + websiteId.length() + 1);
+                    viewString.append("<a href=\"");
+                    if (websiteId != null && request.getRequestURI().contains(websiteId)) {
+                        baseUrl = request.getRequestURI().substring(0,
+                                request.getRequestURI().indexOf(websiteId) + websiteId.length() + 1);
+                    }
+                    if (baseUrl != null) {
+                        viewString.append(baseUrl);
+                    }
+                    viewString.append(genObject.getClass().getSimpleName());
+                    viewString.append("?suri=");
+                    viewString.append(obj.getEncodedURI());
+                    viewString.append("\" ><span class=\"summary-grid glyphicon glyphicon-eye-open\"></span>");
+                    viewString.append("</a>");
                 }
-                if (baseUrl != null) {
-                    viewString.append(baseUrl);
-                }
-                viewString.append(genObject.getClass().getSimpleName());
-                viewString.append("?suri=");
-                viewString.append(obj.getEncodedURI());
-                viewString.append("\" >");
-                }
-                
-            }
-            viewString.append(obj.getProperty(prop));
-            if (showLink) {
-                viewString.append("</a>");
             }
             toReturn = viewString.toString();
         } else if (mode.equals("inlineEdit")) {
@@ -74,10 +68,10 @@ public class TextField extends org.semanticwb.bsc.formelement.base.TextFieldBase
              * dojo.require del InkineEditBox y dijit.form.ValidationTextBox, al menos*/
             StringBuilder viewString = new StringBuilder(128);
             String value = obj.getProperty(prop);
-            String objectId = obj.getSemanticClass().getClassCodeName() + obj.getId() +
-                    propName;
+            String objectId = obj.getSemanticClass().getClassCodeName() + obj.getId()
+                    + propName;
             String url = (String) request.getAttribute("urlRequest");
-            
+
             viewString.append("<script type=\"text/javascript\">\n");
             viewString.append("  <!--\n");
             viewString.append("    var iledit_");
@@ -122,5 +116,5 @@ public class TextField extends org.semanticwb.bsc.formelement.base.TextFieldBase
         }
         return toReturn;
     }
-    
+
 }
