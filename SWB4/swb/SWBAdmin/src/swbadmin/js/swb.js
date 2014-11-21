c0d47458ac84f92e23dc07701ef92dad0f443eb2
cramos <ciramosi@users.noreply.github.com>
2014-11-21 04:24:48 +0000

    Se agregó Array.prototype.every para trabajar con arrays de js

--- SWB4/swb/SWBAdmin/src/swbadmin/js/swb.js
+++ SWB4/swb/SWBAdmin/src/swbadmin/js/swb.js
@@ -1685,6 +1685,30 @@ if (!Array.prototype.indexOf)
     };
 }
 
+/*
+* This prototype is provided by the Mozilla foundation and
+* is distributed under the MIT license.
+* http://www.ibiblio.org/pub/Linux/LICENSES/mit.license
+*/
+if (!Array.prototype.every)
+{
+  Array.prototype.every = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this &&
+          !fun.call(thisp, this[i], i, this))
+        return false;
+    }
+
+    return true;
+  };
+}
 
 /*
  * Funciones de validación de formularios
