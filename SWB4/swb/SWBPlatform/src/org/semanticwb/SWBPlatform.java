94a28d5903212a0b469c00e32f234b8d6434d8d9
jsolis <jsolis@60c509d2-c84e-0410-9847-e8532c4ce32d>
2016-01-21 19:17:03 +0000

    4.5.11.2

--- SWB4/swb/SWBPlatform/src/org/semanticwb/SWBPlatform.java
+++ SWB4/swb/SWBPlatform/src/org/semanticwb/SWBPlatform.java
@@ -128,7 +128,7 @@ public class SWBPlatform
     private static SemanticMgr semanticMgr=null;
 
     /** The Constant version. */
-    protected static final String version = "4.5.11.1";
+    protected static final String version = "4.5.11.2";
 
     /** The m_observers. */
     private ArrayList<ThreadObserver> m_observers = null;
