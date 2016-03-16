9a9c6af8c19bc545520e70cbcb5350181b507c9f
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2016-03-16 18:59:34 +0000

    Creada

---
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/SWBProcess.java
@@ -0,0 +1,29 @@
+package org.semanticwb.process;
+
+public class SWBProcess {
+    public static class JSONProperties {
+        public static final String PROP_CLASS = "class";
+        public static final String PROP_TITLE = "title";
+        public static final String PROP_DESCRIPTION = "description";
+        public static final String PROP_CONNPOINTS = "connectionPoints";
+        public static final String PROP_URI = "uri";
+        public static final String PROP_X = "x";
+        public static final String PROP_Y = "y";
+        public static final String PROP_W = "w";
+        public static final String PROP_H = "h";
+        public static final String PROP_START = "start";
+        public static final String PROP_END = "end";
+        public static final String PROP_PARENT = "parent";
+        public static final String PROP_CONTAINER = "container";
+        public static final String PROP_isMultiInstance = "isMultiInstance";
+        public static final String PROP_isSeqMultiInstance = "isSequentialMultiInstance";
+        public static final String PROP_isCollection = "isCollection";
+        public static final String PROP_isLoop = "isLoop";
+        public static final String PROP_isForCompensation = "isForCompensation";
+        public static final String PROP_isAdHoc = "isAdHoc";
+        public static final String PROP_isTransaction = "isTransaction";
+        public static final String PROP_isInterrupting = "isInterrupting";
+        public static final String PROP_labelSize = "labelSize";
+        public static final String PROP_index = "index";
+    }
+}
