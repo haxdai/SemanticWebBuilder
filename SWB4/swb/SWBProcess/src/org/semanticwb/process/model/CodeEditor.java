7ca6c790b310aac0378a38e0a78ffffcb4a1bb9a
haxdai <haxdai@60c509d2-c84e-0410-9847-e8532c4ce32d>
2016-05-02 16:57:57 +0000

    Modificado orden de inclusiones de estilos para corregir blink en carga (con hipótesis en http://molily.de/domcontentloaded/)

--- SWB4/swb/SWBProcess/src/org/semanticwb/process/model/CodeEditor.java
+++ SWB4/swb/SWBProcess/src/org/semanticwb/process/model/CodeEditor.java
@@ -30,22 +30,22 @@ public class CodeEditor extends org.semanticwb.process.model.base.CodeEditorBase
         String textareaId = obj.getURI()+"/"+propName;
         
         StringBuilder sb = new StringBuilder();
-        sb.append("<link rel=\"stylesheet\" href=\"").append(SWBPlatform.getContextPath()).append("/swbadmin/jsp/process/utils/codemirror/lib/codemirror.css\">")
-          .append("<div dojoType=\"dojox.layout.ContentPane\" executeScripts=\"true\">")
-          .append("  <textarea name=\"").append(propName).append("\" id=\"").append(textareaId).append("\">").append(value).append("</textarea>")
-          .append("  <script type=\"dojo/method\" event=\"postCreate\">")
-          .append("    require({")
-          .append("      packages:[{")
-          .append("        name: \"codemirror\",")
-          .append("        location: \"/swbadmin/jsp/process/utils/codemirror\",")
-          .append("        main: \"lib/codemirror\"")
-          .append("      }]")
-          .append("    },[\"codemirror\",\"codemirror/mode/clike/clike\"], function(CodeMirror) {")
-          .append("      var ").append(editorVar).append(" = CodeMirror.fromTextArea(document.getElementById('").append(textareaId).append("'), ").append(codeMirrorConfig.toString()).append(");")
-          .append(editorVar).append(".on(\"change\", function(cm){document.getElementById('").append(textareaId).append("').value=cm.getValue();});")
-          .append("    });")
-          .append("  </script>")
-          .append("</div>");
+          sb.append("<div dojoType=\"dojox.layout.ContentPane\" executeScripts=\"true\">")
+            .append("  <textarea name=\"").append(propName).append("\" id=\"").append(textareaId).append("\">").append(value).append("</textarea>")
+            .append("  <link rel=\"stylesheet\" href=\"").append(SWBPlatform.getContextPath()).append("/swbadmin/jsp/process/utils/codemirror/lib/codemirror.css\">")
+            .append("  <script type=\"dojo/method\">")
+            .append("    require({")
+            .append("      packages:[{")
+            .append("        name: \"codemirror\",")
+            .append("        location: \"/swbadmin/jsp/process/utils/codemirror\",")
+            .append("        main: \"lib/codemirror\"")
+            .append("      }]")
+            .append("    },[\"codemirror\",\"codemirror/mode/clike/clike\"], function(CodeMirror) {")
+            .append("      var ").append(editorVar).append(" = CodeMirror.fromTextArea(document.getElementById('").append(textareaId).append("'), ").append(codeMirrorConfig.toString()).append(");")
+            .append(editorVar).append(".on(\"change\", function(cm){document.getElementById('").append(textareaId).append("').value=cm.getValue();});")
+            .append("    });")
+            .append("  </script>")
+            .append("</div>");
         
         return sb.toString();
     }
